16888c1c482b3ed1627e2b1ec9cf5378b67d3812
Jack Ryan <jack.dixon.ryan@gmail.com>
2018-06-23 13:59:03 -0600

    AJAX functionality added.

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 //Creating button functions
 
-var keyword;
-var year1;
-var year2;
-var records;
-    
-//Search Button click listener
-$("#search-button").click(function() {
+var keyword = "";
+var year1 = "";
+var year2 = "";
 
-    //variables set to values entered.
-    keyword = ($("#keyword").val());
-    year1 = ($("#start-year").val());
-    year2 = ($("#end-year").val());
-    if (isNaN(year1) || isNaN(year2)) {
-        alert("You must enter a valid year number.")
-    };
-    records = ($("#record-number").val());
-    console.log(keyword, year1, year2, records);
+function getInfo() {
+
+    console.log(keyword);
+
+            // Built by LucyBot. www.lucybot.com
+            // if( year field has value){'facet=1'}
+            //     if(facet ==1){
+            //         'facet_fields': var
+            //     }
+
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+    url += '?' + $.param({
+        'api-key': "be051ce4a2914a458d75290b6bccb660",
+        'q': keyword,
+        'begin_date': year1 + '0101',
+        'end_date': year2 + '0101',
+        'hl': true,
+    });
+
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function (result) {
+        console.log(result);
+        console.log(result.response.docs[0].snippet);
+        $("#results").html(result.response.docs[0].headline.main);
+    }).fail(function (err) {
+        throw err;
+    });
+
+
+}
+
+$("#keyword").on('change', function () {
+    keyword = $(this).val();
+    console.log(keyword);
+});
+$("#start-year").on('change', function () {
+    year1 = $(this).val();
+    console.log(keyword);
+});
+$("#end-year").on('change', function () {
+    year2 = $(this).val();
+    console.log(keyword);
 });
 
 $("#clear-button").click(function() {
